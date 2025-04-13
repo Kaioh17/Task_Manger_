c6d19883d8f1a8a5ab7052df0e9198402f74fbf4
Mubaraq Odumeso <mubskill@gmail.com>
2025-04-13 15:32:54 -0500

    Removed dictionary acting as cache

--- src/taskmanager/task_manager.py
+++ src/taskmanager/task_manager.py
@@ -1,37 +1,30 @@
 """To do task save in dictionary"""
 import pandas as pd
+# from helper import val_task_info
 
 
 class TaskManager:
     def __init__(self):
-        #using nested dictionaries
-        self.to_do_dict = {}
+
+        # self.tm_dict = {}
         self.db_tasks  = []
 
-    def task_info(self,user_name):
+    def task_info(self):
         """User adds task to memory """
         while True:
             try:
                 #User inputs
-                task = input("Task name: ").strip().capitalize()
-                # check if task already exists
-                if task in self.to_do_dict: raise ValueError("Task already exists")
-                #checks if task is empty
-                if not task: raise ValueError("Task cannot be empty")
+                task = str(input("Task name: ").strip().capitalize())
+                # val_task_info(task)
+                if not task:
+                    raise ValueError("Task cannot be empty!!")
 
                 """Ask if user wants to enter in more task"""
 
-                """check if user doesn't exist create new slot for the user"""
-                if user_name not in self.to_do_dict:
-                    self.to_do_dict[user_name] = {}
-
-                print("Found user name")
-                self.to_do_dict[user_name][task] = "Pending"
-                    # add task with default status "pending"
-                print(f"{task} successfully entered to user: {user_name}")
+                print(f"{task} successfully added")
                 return task
 
-            except ValueError as e:
+            except (Exception,ValueError) as e:
                 print(f"Error: {e} ")
 
                 retry = input("Do you want to try again?(Y|N) ").strip(). capitalize()
@@ -72,19 +65,19 @@ class TaskManager:
             try:
                 task = input("Enter task: ").strip().capitalize()
                 if not task: raise IndexError("Cannot accept no value") #check if task empty
-                if task not in  self.to_do_dict: raise KeyError("Task not in memory") #make sure task in dictionary
+                if task not in  self.tm_dict: raise KeyError("Task not in memory") #make sure task in dictionary
 
 
                 status = input("--Change status (pending/done): ").strip().capitalize()
                 #makes sure user enters valid status
                 if status.lower() not in "pending""done":
                     raise IndexError("Invalid status! User can only enter(pending/done)")
-                self.to_do_dict[task] = status
+                self.tm_dict[task] = status
 
                 """Set a condition so that when status is set to done its delete from db and cache """
 
 
-                return print(self.to_do_dict)
+                return print(self.tm_dict)
 
             except (KeyError,IndexError) as e:
                 print(f"Error: {e}")
