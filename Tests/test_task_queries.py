edfe28c856304eeb407557c2b383a27f7a816839
Mubaraq Odumeso <mubskill@gmail.com>
2025-05-04 17:47:00 -0500

    Completed tests for current functions

--- Tests/test_task_queries.py
+++ Tests/test_task_queries.py
@@ -2,6 +2,7 @@ import pytest
 from Task_Manger_.src.db.connection_test import TestDB
 from Task_Manger_.src.db.task_queries import TaskQueries
 import bcrypt
+import random
 
 
 @pytest.fixture
@@ -16,45 +17,90 @@ def db():
     test_db.close_database()
 
 #helper function for creating dummy users
-def _create_test_user(name, db):
+def _create_test_user(user_id, name, db):
     # convert password to hash
     raw_pw = '12333'
     hashed_pw = bcrypt.hashpw(raw_pw.encode('utf-8'), bcrypt.gensalt())
     hashed_pw_str = hashed_pw.decode('utf-8')
     # insert user to the table for testing
     db.cur.execute(
-        "INSERT INTO user_table (user_id, user_name,user_password) VALUES (%s,%s , %s);",
-        ('1234', name, hashed_pw_str)
+        "INSERT INTO user_table (user_id, user_name,user_password) VALUES (%s, %s , %s);",
+        ( user_id, name, hashed_pw_str)
     )
     db.conn.commit()
+##helper function to add a test task to the table
+def _add_test_task(monkeypatch,db):
+    monkeypatch.setattr(random, "randint", lambda a, b: 1010)
+
+    result = TaskQueries(db.conn, db.cur)
+    result.add_task("John", "Testing")
 
 def test_add_task(monkeypatch,db):
     #mock inputs
-    # monkeypatch.setattr(random, "randint", lambda a, b: 1010)
-    _create_test_user("John", db)
-    #
-    #
-    result = TaskQueries(db.conn,db.cur)
-    result.add_task("John", "Testing")
 
-    #
-    peek =  "SELECT user_id FROM task_table WHERE task_name = 'Testing';"
+    _create_test_user(1234,"John", db)
+
+    TaskQueries(db.conn,db.cur)
+    _add_test_task(monkeypatch, db)
+
+    ## check if task is added
+    peek =  "SELECT task_id FROM task_table WHERE task_name = 'Testing';"
     db.cur.execute(peek)
     peeked = db.cur.fetchone()
     db.conn.commit()
 
-    assert peeked[0] == 1234
+    assert peeked[0] == 1010
 
 def test_list_task(monkeypatch, db):
-    pass
+    _create_test_user(1234,"John", db)
+
+    result = TaskQueries(db.conn, db.cur )
+    _add_test_task(monkeypatch, db)
+    # result.add_task("John" , "Testing")
+    list_it = result.list_tasks("John")
+
+    assert list_it == {1010: {'Testing': 'pending'}}  # mock task_id with monkey patch
+
 
 def test_toggle_task_status(monkeypatch,db):
-    pass
+    _create_test_user(1234,"John", db)
+
+    result = TaskQueries(db.conn, db.cur)
+    _add_test_task(monkeypatch, db)
+    status = result.toggle_task_status(1010)
+
+    assert status == "done"
 
 def test_delete_task(monkeypatch,db):
-    pass
+    _create_test_user(1234,"John", db)
+
+    result =  TaskQueries(db.conn, db.cur)
+    _add_test_task(monkeypatch, db)
+    result.delete_task(1010)
+
+    ##check the table to make sure it is empty
+    db.cur.execute("SELECT * FROM task_table")
+    peek = db.cur.fetchone()
+    db.conn.commit()
+
+
+    assert peek is None, "Expected no rows, but got data"
 
 def test_delete_user(monkeypatch,db):
-    pass
+
+    _create_test_user(1234,"John", db)
+
+
+    # perform function
+    result = TaskQueries(db.conn, db.cur)
+    result.del_user(1234)
+
+    ##check if user exists
+    db.cur.execute("SELECT user_id FROM user_table WHERE user_id = '1234'")
+    peek = db.cur.fetchone()
+    db.conn.commit()
+
+    assert peek is None, "Expected no rows, but got data"
+
 
 
